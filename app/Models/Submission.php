b364716679f6a080dc7f9faf53b7d9004516cf11
Philippe Parage <philippe.parage@nc3.lu>
2024-10-31 16:45:16 +0100

    Fixed single user addition to form

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,12 +11,14 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Submission extends Model
 {
     use HasFactory;
-
+    use HasUuids;
     protected $fillable = [
         'form_id',
         'user_id'
     ];
-
+    protected $casts = [
+        'last_edited_at' => 'datetime',
+    ];
     /**
      * Get the form that owns the submission.
      */
