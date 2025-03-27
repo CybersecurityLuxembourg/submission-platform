332dc4f4512ac22ac2c856327eaeabbe86810b30
Philippe Parage <philippe.parage@nc3.lu>
2025-03-27 15:02:32 +0100

    fix: different token creation api key display

--- app/Filament/Resources/ApiTokenResource/Pages/CreateApiToken.php
+++ app/Filament/Resources/ApiTokenResource/Pages/CreateApiToken.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\ApiTokenResource\Pages;
 
 use App\Filament\Resources\ApiTokenResource;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Support\Str;
 
@@ -34,7 +35,24 @@ class CreateApiToken extends CreateRecord
     
     protected function afterCreate(): void
     {
-        // Attach the plain text token to the model for display purposes
+        // Show a notification with the token
+        Notification::make()
+            ->title('API Token Created Successfully')
+            ->body('**COPY THIS TOKEN NOW - it will not be shown again!**<br><code style="user-select: all;">' . $this->plainTextToken . '</code>')
+            ->success()
+            ->persistent()
+            ->actions([
+                \Filament\Notifications\Actions\Action::make('copy')
+                    ->label('Copy Token')
+                    ->icon('heroicon-m-clipboard')
+                    ->extraAttributes([
+                        'x-on:click' => 'navigator.clipboard.writeText(\'' . $this->plainTextToken . '\')',
+                    ])
+                    ->close(),
+            ])
+            ->send();
+            
+        // Also attach it to the record for display on the form
         $this->record->plain_text_token = $this->plainTextToken;
     }
     
